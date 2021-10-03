cfc6f4291c003dbaaeaa6f5bcc36be7b3a933466
Karan Gajjar <karangajjar96@gmail.com>
2021-10-03 00:16:16 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+//import MOCK_DATA as an array
+import JSONDATA from "./MOCK_DATA.json";
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Search user"
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
+      {JSONDATA.filter((val) => {
+        if (searchTerm === "") {
+          return val;
+        } else if (
+          val.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          return val;
+        }
+      }).map((val) => {
+        return (
+          <div className="user" key="key">
+            <div className = 'items'>
+              <img src={val.image} alt="dummy" /> 
+              {val.first_name}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
