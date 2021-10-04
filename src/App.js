5f15b86c1092aadb3f90477c599609dc28f3baa6
Karan Gajjar <karangajjar96@gmail.com>
2021-10-04 14:35:44 -0500

    display book title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import SearchBar from './components/SearchBar';
+import BookData from './Data.json'
 
 function App() {
   return (
     <div className="App">
-      <SearchBar placeholder = "Enter book name" />
+      <SearchBar placeholder = "Enter book name" data = {BookData} />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import "./SearchBar.css";
+import SearchIcon from "@mui/icons-material/Search";
 
-function SearchBar({placeholder, data}) {
+function SearchBar({ placeholder, data }) {
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder = {placeholder}/>
-        <div className="searchIcon"></div>
+        <input type="text" placeholder={placeholder} />
+        <div className="searchIcon">
+          <SearchIcon />
+        </div>
       </div>
-      <div className="dataResult"></div>
+      <div className="dataResult">
+        {data.map((value, key) => {
+          return (
+            <a
+              className="dataItem"
+              href={value.link}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p>{value.title} </p>
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
 }
