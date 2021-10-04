d8741c6fb90b669842ec353bc08abc5e972dacac
Karan Gajjar <karangajjar96@gmail.com>
2021-10-04 15:57:21 -0500

    added search and filter functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
 import SearchIcon from "@mui/icons-material/Search";
 
 function SearchBar({ placeholder, data }) {
+  const [filteredData, setFiltertedData] = useState([]);
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value; //this will search word in the searchbar
+    const newFilter = data.filter((value) => {
+      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
+    setFiltertedData(newFilter);
+  };
+
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} />
+        <input type="text" placeholder={placeholder} onChange={handleFilter}/>
         <div className="searchIcon">
           <SearchIcon />
         </div>
       </div>
-      <div className="dataResult">
-        {data.map((value, key) => {
-          return (
-            <a
-              className="dataItem"
-              href={value.link}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <p>{value.title} </p>
-            </a>
-          );
-        })}
-      </div>
+
+      {filteredData.length !== 0 && (
+        <div className="dataResult">
+          {filteredData.map((value, key) => {
+            return (
+              <a
+                className="dataItem"
+                href={value.link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p>{value.title} </p>
+              </a>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
