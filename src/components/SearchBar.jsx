cf5bdb4a275acb538f3129af2e88c51ee174ce00
Karan Gajjar <karangajjar96@gmail.com>
2021-10-04 21:45:19 -0500

    add clear button and reset functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,51 @@
 import React, { useState } from "react";
 import "./SearchBar.css";
 import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
 
 function SearchBar({ placeholder, data }) {
   const [filteredData, setFiltertedData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
 
   const handleFilter = (event) => {
     const searchWord = event.target.value; //this will search word in the searchbar
+    setWordEntered(searchWord);
     const newFilter = data.filter((value) => {
       return value.title.toLowerCase().includes(searchWord.toLowerCase());
     });
-    setFiltertedData(newFilter);
+    if (searchWord === "") {
+      setFiltertedData([]);
+    } else {
+      setFiltertedData(newFilter);
+    }
+  };
+
+  const clearInput = () => {
+    setFiltertedData([]);
+    setWordEntered("");
   };
 
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} onChange={handleFilter}/>
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
         <div className="searchIcon">
-          <SearchIcon />
+          {filteredData.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id="clearBtn" onClick={clearInput} />
+          )}
         </div>
       </div>
 
       {filteredData.length !== 0 && (
         <div className="dataResult">
-          {filteredData.slice(0,15).map((value, key) => {
+          {filteredData.slice(0, 15).map((value, key) => {
             return (
               <a
                 className="dataItem"
